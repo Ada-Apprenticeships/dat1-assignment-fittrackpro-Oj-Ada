e16e0360d09bc00c09be6c0e63ef18b0c0a9ff1a
Oj-Ada <oj.borah@ada.ac.uk>
2025-02-14 11:04:59 +0000

    Counts all the equipment types that are in stock

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -13,6 +13,9 @@ WHERE next_maintenance_date <= date('now', '+30 days');
 -- TODO: Write a query to find equipment due for maintenance
 
 -- 2. Count equipment types in stock
+SELECT type, COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 -- TODO: Write a query to count equipment types in stock
 
 -- 3. Calculate average age of equipment by type (in days)
