e04753286b607d94f170d0239227e1a53b41b771
Oj-Ada <oj.borah@ada.ac.uk>
2025-02-14 12:27:59 +0000

    Listed all staff members by role, results are ordered by the position to organize the output by the type of role each staff member holds.

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -7,6 +7,9 @@
 -- Staff Management Queries
 
 -- 1. List all staff members by role
+SELECT staff_id, first_name, last_name, position AS role
+FROM staff
+ORDER BY position;
 -- TODO: Write a query to list all staff members by role
 
 -- 2. Find trainers with one or more personal training session in the next 30 days
