992a8da53fdb314b1e4f0b3a06ef36586e95e863
Oj-Ada <oj.borah@ada.ac.uk>
2025-02-14 12:12:15 +0000

    Found the busiest day of the week by the visit coung through using strftime then converting the numbers into actual days of the week.

--- src/attendance_tracking.sql
+++ src/attendance_tracking.sql
@@ -18,6 +18,24 @@ WHERE member_id = 5;
 -- TODO: Write a query to retrieve a member's attendance history
 
 -- 3. Find the busiest day of the week based on gym visits
+SELECT 
+    CASE strftime('%w', check_in_time)
+        WHEN '0' THEN 'Sunday'
+        WHEN '1' THEN 'Monday'
+        WHEN '2' THEN 'Tuesday'
+        WHEN '3' THEN 'Wednesday'
+        WHEN '4' THEN 'Thursday'
+        WHEN '5' THEN 'Friday'
+        WHEN '6' THEN 'Saturday'
+    END AS day_of_week,
+    COUNT(*) AS visit_count
+FROM 
+    attendance
+GROUP BY 
+    day_of_week
+ORDER BY 
+    visit_count DESC
+LIMIT 1;
 -- TODO: Write a query to find the busiest day of the week based on gym visits
 
 -- 4. Calculate the average daily attendance for each location
